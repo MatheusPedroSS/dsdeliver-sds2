653739ff541f1ba5f54ae24ea6be141043a7a536
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-01-11 19:46:58 -0300

    Add orders list

--- front-mobile/src/components/Home/index.tsx
+++ front-mobile/src/components/Home/index.tsx
@@ -1,16 +1,19 @@
-import { OpenSans_700Bold } from '@expo-google-fonts/open-sans';
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import Header from '../Header';
 
 function Home() {
+    const navigation = useNavigation();
 
     const handleOnPress = () => {
-
+        navigation.navigate('Orders');
     }
 
     return (
         <>
+            <Header />
             <View style={styles.container}>
                 <Image source={require('../../assets/deliveryman.png')} />
                 <Text style={styles.title}>
